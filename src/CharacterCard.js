8de552ad71e7ac728204c86448bfde7c48ade4af
pandora024 <phin-phis@hotmail.com>
2020-09-11 12:31:57 +0700

    check2

---
+++ src/CharacterCard.js
@@ -0,0 +1,17 @@
+import './App.css'
+import React, { useState } from 'react';
+export default function CharacterCard(props) {
+ const [active, setActive] = useState(false);
+ const activate = () => {
+    if(!active){
+    setActive(true)
+    props.activationHandler(props.value)
+    }
+    }
+   
+ const className = `card ${active ? 'activeCard': ''}`
+ return (
+ <div className={className} onClick={activate}>{props.value}</div>
+ )
+
+}
---
+++ src/WordCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterCard from './CharacterCard'
+import './App.css'
+const activationHandler = c => { console.log(`${c} has been activated.`) }
+
+export default function WordCard(props){
+ return (
+ <div>
+{ Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}/>) }
+ </div>
+ );
+}
